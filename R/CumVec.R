a148850ca188560e2cb382418eac58894098cea7
MAGALA-RICHARD <105811040+MAGALA-RICHARD@users.noreply.github.com>
2022-07-05 22:46:57 -0500

    Update CumVec.R

--- R/CumVec.R
+++ R/CumVec.R
@@ -1,7 +1,7 @@
 #' CumVec This function allow you to add a vector in a squence to get cumulative values
 #' @param x a vector to increment
 #' @return a vector of incremented values in a sequence relative to the paretn vector
-#'
+#' @export
 CumVec <- function(x){
   add_vec <- function(x) Reduce("+", x, accumulate =TRUE)
   cumx <- eval.parent(substitute(add_vec(x)))
